31d73207d9ee91a1f0d38dfa2ef5c0a75135ac0d
babotavo419 <one.faith419@gmail.com>
2023-08-07 06:45:22 -0400

    routers

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,28 @@
-// DO YOUR MAGIC
+const router = require('express').Router();
+const Cars = require('./cars-model');
+const { checkCarId, checkCarPayload } = require('./cars-middleware');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Cars.getAll();
+    res.json(cars);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get('/:id', checkCarId, (req, res) => {
+  res.json(req.car);
+});
+
+router.post('/', checkCarPayload, async (req, res, next) => {
+  try {
+    const newCar = await Cars.create(req.body);
+    res.status(201).json(newCar);
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
+
