f8cf2f75cc8c8439e7cf998e58646be97f43107f
babotavo419 <one.faith419@gmail.com>
2023-08-07 06:46:11 -0400

    server

--- api/server.js
+++ api/server.js
@@ -1,7 +1,29 @@
-const express = require("express")
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 
-const server = express()
+// Import the cars router
+const carsRouter = require('./api/cars/cars-router');
 
-// DO YOUR MAGIC
+const server = express();
 
-module.exports = server
+// Use helmet and cors middleware for security and cross-origin requests
+server.use(helmet());
+server.use(cors());
+
+// Enable express to parse JSON
+server.use(express.json());
+
+// Use the cars router for all requests starting with '/api/cars'
+server.use('/api/cars', carsRouter);
+
+// Basic error handling middleware
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message,
+    stack: err.stack,
+  });
+});
+
+module.exports = server;
+
