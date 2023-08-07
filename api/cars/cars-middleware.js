6b898a4a1d14e4650ed7c59c77ec6158112d05f2
babotavo419 <one.faith419@gmail.com>
2023-08-07 06:44:36 -0400

    middleware

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,15 +1,34 @@
-const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+const Cars = require('./cars-model');
+
+async function checkCarId(req, res, next) {
+  try {
+    const car = await Cars.getById(req.params.id);
+    if (!car) {
+      res.status(404).json({ message: `car with id ${req.params.id} is not found` });
+    } else {
+      req.car = car;
+      next();
+    }
+  } catch (err) {
+    next(err);
+  }
 }
 
-const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+function checkCarPayload(req, res, next) {
+  const { vin, make, model, mileage } = req.body;
+  if (!vin || !make || !model || !mileage) {
+    res.status(400).json({ message: `${!vin ? 'vin' : !make ? 'make' : !model ? 'model' : 'mileage'} is missing` });
+  } else {
+    next();
+  }
 }
 
-const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+// TODO: Implement validation and uniqueness checks for the vin number
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
-}
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  // checkVinNumberValid,
+  // checkVinNumberUnique,
+};
+
