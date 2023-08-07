ae861e3c4a5974c19dbf18c63d54f2f391640583
babotavo419 <one.faith419@gmail.com>
2023-08-07 06:42:01 -0400

    migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,16 @@
-exports.up = function (knex) {
-  // DO YOUR MAGIC
+exports.up = function(knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('vin', 17).unique().notNullable();
+    tbl.string('make').notNullable();
+    tbl.string('model').notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+  });
 };
 
-exports.down = function (knex) {
-  // DO YOUR MAGIC
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('cars');
 };
+
